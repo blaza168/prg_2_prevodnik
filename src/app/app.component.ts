600e2fb9e0c009987b76dfce6dbf019704db4702
jan.blazek <JBlazekk@seznam.cz>
2019-04-02 17:10:29 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'prg2prevod';
+  public form: FormGroup;
+
+  public result: {from: string, to: number};
+
+  constructor() {
+    this.form = this.buildForm();
+  }
+
+  public onSubmit(): void {
+    const from: string = this.form.value.number;
+    let result = 0;
+    for (let i = 0; i < from.length; i++) {
+      if (from.charAt(from.length - 1 - i) === '1') {
+        result += Math.pow(2, i);
+      }
+    }
+
+    this.result = {from, to: result};
+  }
+
+  private buildForm(): FormGroup {
+    return new FormGroup({
+      number: new FormControl(null, [Validators.required, Validators.pattern(
+        new RegExp('^[0-1]+$')
+      )]),
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
