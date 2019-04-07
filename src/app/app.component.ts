d802fdc2d39750b002500a5ee2ddf2b24b82ac04
jan.blazek <JBlazekk@seznam.cz>
2019-04-07 14:48:44 +0200

    Implement real-time calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,32 +8,36 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public form: FormGroup;
 
+  public input: FormControl;
   public result: {from: string, to: number};
 
   constructor() {
-    this.form = this.buildForm();
+    this.input = this.buildControl();
   }
 
-  public onSubmit(): void {
-    const from: string = this.form.value.number;
+  private toBinary(binary: string): {from: string, to: number} {
     let result = 0;
-    for (let i = 0; i < from.length; i++) {
-      if (from.charAt(from.length - 1 - i) === '1') {
+    for (let i = 0; i < binary.length; i++) {
+      if (binary.charAt(binary.length - 1 - i) === '1') {
         result += Math.pow(2, i);
       }
     }
 
-    this.result = {from, to: result};
+    return {from: binary, to: result};
   }
 
-  private buildForm(): FormGroup {
-    return new FormGroup({
-      number: new FormControl(null, [Validators.required, Validators.pattern(
+  private buildControl(): FormControl {
+    const control = new FormControl(null, [Validators.pattern(
         new RegExp('^[0-1]+$')
-      )]),
+      )]);
+    control.markAsTouched();
+    control.valueChanges.subscribe((value: string) => {
+      if (this.input.valid) {
+        this.result = this.toBinary(value);
+      }
     });
+    return control;
   }
 
 }
